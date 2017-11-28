4201be10a88538c0c3f5576084d6622cac7d9fb5
Anand J Bangad <ajbangad@vt.edu>
2017-11-28 16:16:26 -0500

    SQL updates

--- src/main/java/userlog/Recommendation.java
+++ src/main/java/userlog/Recommendation.java
@@ -42,35 +42,44 @@ public class Recommendation {
                         }
                     }
                     if (count >= 3) {
+                        String m ="";
+                        String n="";
                         for (String item : keywords.get(j)) {
                             if ((!keywords.get(i).contains(item))) {
                                 recommenderList.add(i, item);
                                 System.out.println("Recommended Item for" + " " + userID.get(i) + " " + recommenderList.get(i));
-//                                String sql1 = "INSERT INTO Recommendations " + "VALUES ()";
+                                m = m + recommenderList.get(i);
+                                PreparedStatement myPstm = myConn.prepareStatement("UPDATE user_log SET RECOMMENDATIONS = ? WHERE UserID = ?");
+                                myPstm.setString(1,m);
+                                myPstm.setString(2,userID.get(i));
+                                myPstm.executeUpdate();
+//                                  String sql1 = "INSERT INTO Recommendations " + "VALUES ()";
 //                                String sql2 = recommenderList.get(i);
 //                                myStmt.executeUpdate(sql1 + "VALUES (" + sql2 + ")");
 //                                PreparedStatement ps = myConn.prepareStatement("insert into user_log(Recommendations) values(?)");
 //                                ps.setString(0,recommenderList.get(i));
                             }
-//                            String sql = "INSERT INTO user_log(UserID,Keywords,Recommendations) VALUES(?,?,?)";
-//                            String m = null;
-//                            m = m + "," + recommenderList.get(i);
+                        }
+//                            String sql = "INSERT INTO user_log(Recommendations) VALUES(?) WHERE  (UserId =?)";
 //                            try (
+//
 //                                PreparedStatement pstmt = myConn.prepareStatement(sql)) {
-//                                pstmt.setString(1, null);
-//                                pstmt.setString(2, null);
-//                                pstmt.setString(3, m);
+//                                pstmt.setString(1, m+recommenderList.get(i));
+//                                pstmt.setString(2, userID.get(i));
 //                                pstmt.executeUpdate();
 //                            }catch (SQLException e) {
 //                                    System.out.println(e.getMessage());
 //                                }
-//
 
-                            }
                         for (String item : keywords.get(i)) {
                             if (!keywords.get(j).contains(item)) {
                                 recommenderList.add(j, item);
                                 System.out.println("Recommended Item for" +" " + userID.get(j) + " " + recommenderList.get(j));
+                                n = n + recommenderList.get(j);
+                                PreparedStatement myPstm = myConn.prepareStatement("UPDATE user_log SET RECOMMENDATIONS = ? WHERE UserID = ?");
+                                myPstm.setString(1,n);
+                                myPstm.setString(2,userID.get(j));
+                                myPstm.executeUpdate();
 //                                String sql1 = "INSERT INTO Recommendations " + "VALUES ()";
 //                                String sql2 = recommenderList.get(j);
 //                                myStmt.executeUpdate(sql1 + "VALUES (" + sql2 + ")");
